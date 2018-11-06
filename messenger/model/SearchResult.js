d8ca1cb41e222af3034caef989d4dd5ce9915050
tutanck <tutanck@gmail.com>
2018-11-06 11:25:24 +0100

    no more default action

--- messenger/model/SearchResult.js
+++ messenger/model/SearchResult.js
@@ -5,12 +5,6 @@ function SearchResult(context, title, subtitle, action_url, image_url) {
     title: title,
     subtitle: subtitle,
     image_url: image_url,
-    /* default_action: {
-      type: "web_url",
-      url: action_url,
-      messenger_extensions: false,
-      webview_height_ratio: "tall"
-    }, */
     buttons: [
       {
         type: "web_url",
