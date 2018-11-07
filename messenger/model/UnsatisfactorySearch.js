1642cb98256ec0f21d2982936d39d1d2ca1ff2a6
tutanck <tutanck@gmail.com>
2018-11-07 16:54:32 +0100

    useless decode package deletion

--- messenger/model/UnsatisfactorySearch.js
+++ messenger/model/UnsatisfactorySearch.js
@@ -1,10 +1,6 @@
-const decode = require("urldecode");
-
 const PostbackPayload = require("./PostbackPayload");
 
 function UnsatisfactorySearch(context, text) {
-  const ctx = decode(context);
-
   return {
     attachment: {
       type: "template",
@@ -15,17 +11,17 @@ function UnsatisfactorySearch(context, text) {
           {
             type: "postback",
             title: "Reformuler la recherche",
-            payload: PostbackPayload(ctx, "start_search")
+            payload: PostbackPayload(context, "start_search")
           },
           {
             type: "web_url",
             title: "Partager sur Workplace",
-            url: `https://work.facebook.com/sharer.php?display=popup&u=https://faq.zenika.com/?q=${ctx}&quote=${ctx}`
+            url: `https://work.facebook.com/sharer.php?display=popup&u=https://faq.zenika.com/?q=${context}&quote=${context}`
           },
           {
             type: "web_url",
             title: "Rechercher dans FAQ",
-            url: `https://faq.zenika.com/?q=${ctx}`
+            url: `https://faq.zenika.com/?q=${context}`
           }
         ]
       }
