962e23d0870d7aa6e45fd6134896bf1aae8aa039
tutanck <tutanck@gmail.com>
2018-11-13 17:16:18 +0100

    test template SearchResult

--- slack/model/SearchResult.js
+++ slack/model/SearchResult.js
@@ -1,9 +1,13 @@
 function SearchResult(title, subtitle, action_url, image_url) {
   return {
-    title: `${title}\n${action_url}`,
+    title: `${title}`,
+    title_link: action_url,
     text: subtitle,
     image_url: image_url,
-    color: "#af1e3a"
+    color: "#af1e3a",
+    footer: "Slack API",
+    footer_icon: "🤔",
+    ts: 123456789
   };
 }
 
