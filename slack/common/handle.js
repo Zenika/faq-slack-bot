dad5760e98e1fe97527cd829437ceb9d69387cbf
tutanck <tutanck@gmail.com>
2018-11-13 14:28:06 +0100

    _

--- slack/common/handle.js
+++ slack/common/handle.js
@@ -1,16 +1,17 @@
-const SearchResult = require("../model/SearchResult");
+const Caroussel = require("../model/Caroussel"),
+  SearchResult = require("../model/SearchResult");
 
 // Handles command events
 function handleCommand({ text: commandText, ...meta }) {
   console.log("handleCommand", "text:", commandText, "meta:", meta);
 
-  const result = SearchResult("LOL", commandText, "subtitle");
+  const result = Caroussel("LOL", [SearchResult("titre")]);
 
-  // Checks if the command's text exists
+  /* // Checks if the command's text exists
   if (commandText) {
     //TODO search for the query string
     //TODO limit to 9 results
-  }
+  } */
 
   return result;
 }
---
+++ slack/model/Caroussel.js
@@ -0,0 +1,8 @@
+function Caroussel(context, searchResultList = []) {
+  return {
+    text: context,
+    attachments: searchResultList
+  };
+}
+
+module.exports = Caroussel;
