88489b4a47d74e3ba80647c113f5681279bb06ed
tutanck <tutanck@gmail.com>
2018-11-14 10:08:48 +0100

    Zenika brand

--- slack/model/SearchResult.js
+++ slack/model/SearchResult.js
@@ -5,8 +5,8 @@ function SearchResult(title, subtitle, action_url, image_url) {
     text: subtitle,
     image_url: image_url,
     color: "#af1e3a",
-    footer: "Slack API",
-    footer_icon: "🤔",
+    footer: "faq.zenika.com",
+    footer_icon: "https://faq.zenika.com/img/favicon/favicon-64.png",
     ts: 123456789
   };
 }
