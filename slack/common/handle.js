895909112bad6821e5bebf1a1d3839c90077b637
tutanck <tutanck@gmail.com>
2018-11-14 12:04:34 +0100

    7 search results

--- slack/common/handle.js
+++ slack/common/handle.js
@@ -15,7 +15,7 @@ function handleCommand({ text: commandText, ...meta }) {
         const { search } = await faq(commandText);
 
         if (search.nodes && search.nodes.length > 0) {
-          message = makeCaroussel(commandText, search.nodes, 5);
+          message = makeCaroussel(commandText, search.nodes, 7);
           console.log("caroussel:", message);
         } else {
           message = {
