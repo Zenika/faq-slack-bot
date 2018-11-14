8887e41c8a1f9360d1295f0b82b3699a8a30ea36
tutanck <tutanck@gmail.com>
2018-11-14 14:58:02 +0100

    slack::UnsatisfactorySearch

--- slack/model/UnsatisfactorySearch.js
+++ slack/model/UnsatisfactorySearch.js
@@ -2,12 +2,21 @@ function UnsatisfactorySearch(context, text) {
   return {
     text: text,
     attachments: [
-      { text: "Reformuler la recherche" },
+      {
+        text: "Reformuler la recherche",
+        fields: [
+          {
+            title: "Tu peux toujours faire ça :",
+            value: "Reformuler la recherche."
+          }
+        ]
+      },
       {
         attachment_type: "default",
-        text: "Tu peux toujours faire ça :",
+        text: "Tu peux aussi faire ça :",
         fallback: "Je n'ai pas trouvé ce que je cherche..",
         callback_id: "unsatisfactory_search",
+        color: "#3AA3E3",
         actions: [
           {
             type: "button",
