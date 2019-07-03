9ebe2a649d13c1f97919b0abc227567993c4172a
tutanck <tutanck@gmail.com>
2019-07-03 11:35:28 +0200

    _

--- client/slack/index.js
+++ client/slack/index.js
@@ -1,63 +1,52 @@
-const  
-  UnsatisfactorySearch = require('./model/UnsatisfactorySearch'),;
-
-const makeCaroussel = require('./adapter/faq');
+const searchFaq = require('./adapter/faq');
+const searchStack = require('./adapter/stack');
 
 // Handles command events
 function handleCommand(received_command) {
   console.log('handleCommand received_command : ', received_command);
 
-  return new Promise(async (resolve, reject) => {
-    let message;
+  return new Promise(
+    /* async */ (resolve, reject) => {
+      let message;
 
-    // Check if the command is sent with a search text.
-    if (received_command.command && received_command.text) {
-      const { command, text } = received_command;
+      // Check if the command is sent with a search text.
+      if (received_command.command && received_command.text) {
+        const { command, text } = received_command;
 
-      try {
-        switch (command) {
-          case '/faq': // Start a search session for the query string by requesting the FAQ API
-            console.log("Starting a search session from Zenika Faq API...");
-            // Start a search session for the query string by requesting the FAQ's API
-        const { search } = await faq(commandText);
+        try {
+          switch (command) {
+            case '/faq':
+              console.log('Starting a search session from Zenika Faq..');
+              message = searchFaq(text, 5);
+              break;
 
-        if (search.nodes && search.nodes.length > 0) {
-          message = makeCaroussel(commandText, search.nodes, 5);
-          console.log("caroussel:", message);
-        } else {
-          message = UnsatisfactorySearch(
-            commandText,
-            `Désolé! Je n'ai rien trouvé  😭`
-          );
+            case '/stack':
+              console.log('Starting a search session from StackOverflow..');
+              message = searchStack(text, 5);
+              break;
+
+            default:
+              console.log('Unknown command : ', command);
+              message = {
+                text: `Désolé! Une erreur inattendue s'est produite 😱`
+              };
+          }
+        } catch (err) {
+          console.log('handleCommand err : ', err);
+          message = {
+            text: `Désolé! Une erreur inattendue s'est produite 😱`
+          };
         }
-            break;
-
-          case '/stack': // Start a search session for the query string by requesting the StackOverflow API
-            console.log('Starting a search session from StackOverflow API...');
-           
-            break;
-
-          default:
-            console.log('Unknown command : ', command);
-            message = {
-              text: `Désolé! Une erreur inattendue s'est produite 😱`
-            };
-        }
-      } catch (err) {
-        console.log('handleCommand err : ', err);
+      } else {
+        // Use a JSON payload to communicate the error back to the user as an ephemeral message.
         message = {
-          text: `Désolé! Une erreur inattendue s'est produite 😱`
+          text:
+            "La commande /faq doit toujours être suivie d'un texte de recherche. \n ex: /faq comment faire une note de frais"
         };
       }
-    } else {
-      // Use a JSON payload to communicate the error back to the user as an ephemeral message.
-      message = {
-        text:
-          "La commande /faq doit toujours être suivie d'un texte de recherche. \n ex: /faq comment faire une note de frais"
-      };
-    }
 
-    resolve(message);
-  });
+      resolve(message);
+    }
+  );
 }
 module.exports = { handleCommand };
--- client/slack/model/SearchResult.js
+++ client/slack/model/SearchResult.js
@@ -1,19 +1,19 @@
 function SearchResult(
-  context,
   title,
-  subtitle,
-  action_url,
-  image_url,
-  providerUrl
+  text,
+  title_link,
+  providerUrl,
+  providerIconUrl,
+  image_url
 ) {
   return {
-    title: `${title}`,
-    title_link: action_url,
-    text: subtitle,
-    image_url: image_url,
+    title,
+    text,
+    title_link,
+    image_url,
     color: '#af1e3a',
-    footer: `${providerUrl}`,
-    footer_icon: `${providerUrl}/img/favicon/favicon-64.png`
+    footer: providerUrl,
+    footer_icon: providerIconUrl
   };
 }
 
