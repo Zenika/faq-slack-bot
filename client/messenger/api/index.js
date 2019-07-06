386240112ce3b272c5facc26f9b4ec404c18689a
tutanck <tutanck@gmail.com>
2019-07-06 10:21:34 +0200

    _

--- client/messenger/api/index.js
+++ client/messenger/api/index.js
@@ -4,6 +4,7 @@ const request = require("request");
 
 // Sends response messages via the Send API
 function callSendAPI(sender_psid, content) {
+  console.log("callSendAPI", sender_psid, content)
   // Construct the message body
   let request_body = {
     recipient: {
