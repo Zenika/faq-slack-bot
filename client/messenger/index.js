dac8feb9e9c656d18a2e1df409a463eba5a646e6
tutanck <tutanck@gmail.com>
2019-07-06 10:30:43 +0200

    _

--- client/messenger/index.js
+++ client/messenger/index.js
@@ -21,7 +21,7 @@ async function handleMessage(sender_psid, received_message) {
 
       // Start a search session for the query string by querying the FAQ's API
       console.log('Starting a search session from Zenika Faq..');
-      message = await searchFaq(text, 5);
+      message = searchFaq(text, 5);
 
       console.log('returned message : ', message);
     } catch (err) {
